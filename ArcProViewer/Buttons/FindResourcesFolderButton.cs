53b49ada96ce74f62f358859dae950aab008b678
Philip <philip@northarrowresearch.com>
2024-06-21 12:57:36 -0700

    browse resource folder implemented

--- ArcProViewer/Buttons/FindResourcesFolderButton.cs
+++ ArcProViewer/Buttons/FindResourcesFolderButton.cs
@@ -1,22 +1,6 @@
-﻿using ArcGIS.Core.CIM;
-using ArcGIS.Core.Data;
-using ArcGIS.Core.Geometry;
-using ArcGIS.Desktop.Catalog;
-using ArcGIS.Desktop.Core;
-using ArcGIS.Desktop.Editing;
-using ArcGIS.Desktop.Extensions;
-using ArcGIS.Desktop.Framework;
-using ArcGIS.Desktop.Framework.Contracts;
-using ArcGIS.Desktop.Framework.Dialogs;
-using ArcGIS.Desktop.Framework.Threading.Tasks;
-using ArcGIS.Desktop.KnowledgeGraph;
-using ArcGIS.Desktop.Layouts;
-using ArcGIS.Desktop.Mapping;
+﻿using ArcGIS.Desktop.Framework.Contracts;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace ArcProViewer.Buttons
 {
@@ -24,6 +8,18 @@ namespace ArcProViewer.Buttons
     {
         protected override void OnClick()
         {
+            try
+            {
+                string folder = System.IO.Path.Combine(Environment.GetEnvironmentVariable("APPDATA"), Properties.Resources.AppDataFolder);
+                if (System.IO.Path.Exists(folder))
+                {
+                    Process.Start(new ProcessStartInfo(folder) { UseShellExecute = true });
+                }
+            }
+            catch (Exception ex)
+            {
+                //TODO handle exception
+            }
         }
     }
 }
